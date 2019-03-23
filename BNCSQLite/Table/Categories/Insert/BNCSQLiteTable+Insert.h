09a9b1a9f864bbbf64188aff65b97126f2e4a9ad
Jax Wu <bignerdcoding@gmail.com>
2019-03-23 10:00:29 +0800

    add table insert category missing comment

--- BNCSQLite/Table/Categories/Insert/BNCSQLiteTable+Insert.h
+++ BNCSQLite/Table/Categories/Insert/BNCSQLiteTable+Insert.h
@@ -13,9 +13,23 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface BNCSQLiteTable (Insert)
 
+/**
+ insert a record
+
+ @param record the record to insert
+ @param error the error if insert fails
+ @return return YES if success
+ */
 - (BOOL)insertRecord:(NSObject<BNCSQLiteRecordProtocol> *)record
                error:(NSError *__nullable __autoreleasing *)error;
 
+/**
+ insert a list of record
+
+ @param recordList the list of record to insert
+ @param error the error if insert fails
+ @return return YES if success
+ */
 - (BOOL)insertRecordList:(NSArray<NSObject<BNCSQLiteRecordProtocol> * > *)recordList
                    error:(NSError *__nullable __autoreleasing *)error;
 
